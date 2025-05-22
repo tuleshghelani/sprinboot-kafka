39120de024fb688b72aa04c19965b698024d0929
tuleshghelani <ghelanitulesh@gmail.com>
2025-05-22 12:06:05 +0530

    Upgrade Spring Boot version to 3.3.3 and Java version to 17; refactor KafkaProducer to use CompletableFuture for asynchronous message sending.

--- src/main/java/com/trim/kafkarest/services/KafkaProducer.java
+++ src/main/java/com/trim/kafkarest/services/KafkaProducer.java
@@ -8,8 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
-import org.springframework.util.concurrent.ListenableFuture;
-import org.springframework.util.concurrent.ListenableFutureCallback;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class KafkaProducer {
@@ -25,17 +24,13 @@ public class KafkaProducer {
 	public void send(String data) {
 		LOG.info("Sending data: {}", data);
 		
-		ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send(topic, data);
+		CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(topic, data);
 		
-		future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
-			@Override
-			public void onSuccess(SendResult<String, String> result) {
+		future.whenComplete((result, ex) -> {
+			if (ex == null) {
 				LOG.info("Successfully sent message: [{}] with offset: [{}]", 
 						data, result.getRecordMetadata().offset());
-			}
-
-			@Override
-			public void onFailure(Throwable ex) {
+			} else {
 				LOG.error("Failed to send message: [{}]", data, ex);
 			}
 		});
